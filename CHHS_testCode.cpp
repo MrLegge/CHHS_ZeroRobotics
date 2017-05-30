4c0d60db1340a2f20073b8d01829b3f0ece0e888
Mr Phil Legge <phil.legge@det.nsw.edu.au>
2017-05-30 12:47:49 +1000

    Create CHHS_testCode.cpp

---
+++ CHHS_testCode.cpp
@@ -0,0 +1,12 @@
+//Declare any variables shared between functions here
+
+void init(){
+	//This function is called once when your code is first loaded.
+
+	//IMPORTANT: make sure to set any variables that need an initial value.
+	//Do not assume variables will be set to 0 automatically!
+}
+
+void loop(){
+	//This function is called once per second.  Use it to control the satellite.
+}
