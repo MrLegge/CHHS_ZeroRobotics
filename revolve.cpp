5b01d00950698a5102046b75948f0274ffc00f9b
Mr Phil Legge <phil.legge@det.nsw.edu.au>
2017-06-15 14:07:11 +1000

    Create revolve.cpp

--- revolve.cpp
+++ revolve.cpp
@@ -15,8 +15,8 @@ void init(){
 void loop(){
 	//This function is called once per second.  Use it to control the satellite.
 	if (count <= 11){
-		DEBUG(("myPosition %d",myPosition[count]));
-		DEBUG(("centralPosition %d",centralPosition[count]));
+		DEBUG(("myPosition %f",myPosition[count]));
+		DEBUG(("centralPosition %f",centralPosition[count]));
 	}
 	count++;
 }
