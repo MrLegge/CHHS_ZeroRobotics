b7bdba616e312f201e4dd9293b1b81b06f0bc57c
Mr Phil Legge <phil.legge@det.nsw.edu.au>
2017-07-19 14:25:15 +1000

    Update hasReachedTargetPosition.cpp

--- hasReachedTargetPosition.cpp
+++ hasReachedTargetPosition.cpp
@@ -2,21 +2,12 @@
 //Declare any variables that need to be shared within this block  
 float myPosition[12];
 float targetPosition[12];
-int count; 
-void init(){
-	//set all values in array to zero
-	for(int i=0;i<12;i++){
-		myPosition[i]=0.0f;
-		targetPosition[i]=0.0f;
-	}
-	count = 0;
-}
 
-void loop(){
-	//This function is called once per second.  Use it to control the satellite.
-	if (count <= 11){
+Boolean hasReachedTargetPosition(){
+	//calculate if we are close enough to the spot we want to be
+	if (are we close enough to target){
 		DEBUG(("myPosition %f",myPosition[count]));
 		DEBUG(("targetPosition %f",targetPosition[count]));
 	}
-	count++;
+	
 }
