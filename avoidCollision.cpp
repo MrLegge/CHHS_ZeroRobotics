632acb0f57dfe9d1d773a4205b8485ecac5212ed
Mr Phil Legge <phil.legge@det.nsw.edu.au>
2017-07-19 11:56:24 +1000

    Update avoidCollision.cpp

--- avoidCollision.cpp
+++ avoidCollision.cpp
@@ -1,22 +1,13 @@
 //This is a dummy file to start off work on the function that monitors the distance between two or more objects to avoid collisions.
 //Declare any variables that need to be shared within this block  
 float myPosition[12];
-float otherPosition[12];
-int count; 
-void init(){
-	//set all values in array to zero
-	for(int i=0;i<12;i++){
-		myPosition[i]=0.0f;
-		otherPosition[i]=0.0f;
-	}
-	count = 0;
-}
+float otherObjectPosition[12];
 
-void loop(){
+
+void avoidCollision(){
 	//This function is called once per second.  Use it to control the satellite.
-	if (count <= 11){
-		DEBUG(("myPosition %f",myPosition[count]));
-		DEBUG(("otherPosition %f",otherPosition[count]));
+	if (myPosition is too close to otherObjectPosition || my Vector will intercept otherObjects vector){ // fix for these ideas
+		DEBUG(("myPosition %f",myPosition[1])); 
+		DEBUG(("otherPosition %f",otherObjectPosition[1]));
 	}
-	count++;
 }
